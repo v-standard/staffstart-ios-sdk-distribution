4a17e9609af909f87be26484ab32f174fa5fefc0
ohayoukenchan <san.kun.sakana@gmail.com>
2024-11-06 16:03:04 +0900

    :construction: 試しにcore, tracking, appをあげる

---
+++ StaffStart__Core.xcframework/ios-arm64/StaffStart__Core.framework/Headers/StaffStartCore.h
@@ -0,0 +1,19 @@
+//
+//  StaffStart__Core.h
+//  StaffStart__Core
+//  
+//  Created by ohayoukenchan on 2024/10/17.
+//  
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StaffStart__Core.
+FOUNDATION_EXPORT double StaffStart__CoreVersionNumber;
+
+//! Project version string for StaffStart__Core.
+FOUNDATION_EXPORT const unsigned char StaffStart__CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StaffStart__Core/PublicHeader.h>
+
+
