ff33a86077966ba8f9edc634f75fa6fcfc282ab4
ohayoukenchan <san.kun.sakana@gmail.com>
2025-04-02 12:53:40 +0900

    :punch: v1.2.0

---
+++ Example/Example/StaffStartApp/TabView/ProductTabView.swift
@@ -0,0 +1,40 @@
+//
+//  ProductTabView.swift
+//  Example
+//
+//  Created by ohayoukenchan on 2025/03/14.
+//  Copyright © 2025 VANISH STANDARD. All rights reserved.
+//
+import StaffStart__App
+import StaffStart__Core
+import StaffStart__Tracking
+import SwiftUI
+
+struct ProductTabView: View {
+    var baseProductCode: String?
+
+    var onTapReadMore: (_ baseProductCode: String) -> Void
+
+    var onTapSnapPlay: (_ cid: Int) -> Void
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Text("商品詳細").font(.title).padding(.bottom, 8)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Spacer()
+            if let baseProductCode {
+                StaffStartSnapPlayBlockView(coordinateListParams: CoordinateListParams(
+                    baseProductCode: baseProductCode
+                ), onTapReadMore: { baseProductCode in
+                    onTapReadMore(baseProductCode)
+                }, onTapSnapPlay: { cid in
+                    onTapSnapPlay(cid)
+                    Task {
+                        await Tracker.shared.trackPageView(with: cid)
+                    }
+                })
+            }
+        }
+        .padding()
+    }
+}
