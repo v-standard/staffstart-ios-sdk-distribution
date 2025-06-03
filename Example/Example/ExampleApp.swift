aa27328c76c4bd12c8be57d56bbf141b770e38fe
ohayoukenchan <san.kun.sakana@gmail.com>
2025-06-03 21:19:22 +0900

    :punch: release 1.4.0

--- Example/Example/ExampleApp.swift
+++ Example/Example/ExampleApp.swift
@@ -16,7 +16,7 @@ struct ExampleApp: App {
 
     var body: some Scene {
         WindowGroup {
-            StaffStartApp()
+            MainTabView()
             /// 元の画面遷移を伴うUIを利用する場合はExampleView()を使ってください
             // ExampleView()
             /// APIの方を確認するにはContentView()を使ってください
